b761341517de1bdef6a770a6bada8c0d5cd26a77
Mateo <mateoquinteros2@gmail.com>
2021-06-23 14:48:24 -0600

    fondo

--- application/views/menuestudiante.php
+++ application/views/menuestudiante.php
@@ -37,7 +37,7 @@ Coded by www.creative-tim.com
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 
-<body class="">
+<body class="" style="background-color:#f4f3ef;">
   <div class="wrapper ">
     <div class="sidebar" data-color="white" data-active-color="danger">
       <div class="logo">
